93128586cffeca51ed6e7f9f1c9a21801dcddcfe
Wamitinewton <wamitinewton@gmail.com>
2024-06-22 16:06:56 +0300

    first server commit

---
+++ src/main/kotlin/com/athena/plugins/Routing.kt
@@ -0,0 +1,13 @@
+package com.athena.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
